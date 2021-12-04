c417e27fba11d75b602277068780c26a1e8afd7f
mr150 <im150@bk.ru>
2021-12-05 00:46:18 +0300

    feat: add `Modal` component

---
+++ src/components/modal/index.js
@@ -0,0 +1,27 @@
+import React from "react";
+import propTypes from 'prop-types';
+import './style.css';
+
+function Modal({onClose, hidden, title, children}){
+  console.log('Modal');
+
+  return (
+    <div className='overlay' hidden={hidden}>
+      <div className='Modal'>
+        <header className='Modal__header'>
+          <h2 className='Modal__title'>{title}</h2>
+          <button onClick={() => onClose()}>Закрыть</button>
+        </header>
+        {children}
+      </div>
+    </div>
+  );
+}
+
+Modal.propTypes = {
+  hidden: propTypes.bool,
+  title: propTypes.string,
+  onClose: propTypes.func.isRequired,
+};
+
+export default React.memo(Modal);
